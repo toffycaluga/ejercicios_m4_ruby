3231bba78891744ae2c4b4d4ec6edff7a59712bb
Toffy Caluga <abraham.lillol@gmail.com>
2023-06-05 11:58:05 -0300

    agregado loop.rb

---
+++ flujo_ciclo_metodos/dia_2/loop.rb
@@ -0,0 +1,12 @@
+
+# puts "Programando con Ruby"
+# puts "Programando con Ruby"
+# puts "Programando con Ruby"
+# puts "Programando con Ruby"
+# puts "Programando con Ruby"
+
+count = 0
+while i < 5 
+    puts "Programando con Ruby"
+    count += 1
+end
